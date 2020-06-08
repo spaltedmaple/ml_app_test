721fa89a80decc1efa049c8d9920b9c8dbf38bd1
spaltedmaple <62574045+spaltedmaple@users.noreply.github.com>
2020-06-07 22:59:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def main():
 
     @st.cache(persist=True)
     def load_data():
-        data = pd.read_csv("C:/Users/deep.ai/Desktop/DS_projects/ML_app_project/mushrooms.csv")
+        data = pd.read_csv("mushrooms.csv")
         labelencoder=LabelEncoder()
         for col in data.columns:
             data[col] = labelencoder.fit_transform(data[col])
